603075eefba1581562be29fd9d63d65a02f937ea
Alejandro Lopez <alejozepol@gmail.com>
2019-10-28 22:43:02 -0500

    creacion de metodos para consulta a la base de datos

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -5,17 +5,20 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const DB_NAME = config.dbName;
 
-const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
+/* const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`; */
+const MONGO_URI = `mongodb+srv://PlatziStore:Si5q66Nbee9KBGC@reto09-2fb4y.mongodb.net/test?retryWrites=true&w=majority`;
 
 class MongoConnect {
   constructor() {
     this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
     this.dbName = DB_NAME;
+    console.log(MONGO_URI)
+
   }
 
   connect() {
-    if (!MongoLib.connection) {
-      MongoLib.connection = new Promise((resolve, reject) => {
+    if (!MongoConnect.connection) {
+      MongoConnect.connection = new Promise((resolve, reject) => {
         this.client.connect(err => {
           if (err) {
             reject(err);
@@ -25,7 +28,44 @@ class MongoConnect {
         });
       });
     }
-    return MongoLib.connection;
+    return MongoConnect.connection;
+  }
+  
+  getAll(collection, query) {
+    return this.connect().then(db => {
+      return db
+        .collection(collection)
+        .find(query)
+        .toArray();
+    });
+  }
+  get(collection, id) {
+    return this.connect().then(db => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
+  }
+  create(collection, data) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).insertOne(data);
+      })
+      .then(result => result.insertedId);
+  }
+  update(collection, id, data) {
+    return this.connect()
+      .then(db => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+      })
+      .then(result => result.upsertedId || id);
+  }
+  delete(collection, id) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+      })
+      .then(() => id);
   }
 }
 
